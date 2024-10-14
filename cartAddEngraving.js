55a3d531cabefb8bd42e33c2d5ecdcd4f82d8eea
camct <110494683+camct@users.noreply.github.com>
2024-10-13 21:18:07 -0600

    More detailed and cleaner error handling.  Fixed cart update.

--- cartAddEngraving.js
+++ cartAddEngraving.js
@@ -3,24 +3,25 @@ Ecwid.OnAPILoaded.add(function() {
       console.log('Page type is', page.type, "!!!");
       if (page.type === 'PRODUCT') {
           console.log(page.productId);
-          var productIds = [55001151, 74102380, 506210440, 570262509, 94782479];
+          const productIds = [55001151, 74102380, 506210440, 570262509, 94782479];
   
           // Check if the current product ID is in the allowed list
           if (!productIds.includes(page.productId)) {return;}
   
-          var basePrices = {55001151: 119.95, 74102380: 131.95, 506210440: 136.95, 570262509: 119.95, 94782479: 71.00};
-          var basePrice = basePrices[page.productId];
+          const basePrices = {55001151: 119.95, 74102380: 131.95, 506210440: 136.95, 570262509: 119.95, 94782479: 71.00};
+          const basePrice = basePrices[page.productId];
   
           // Find the engraving input field
-          var engravingInput1 = document.querySelector("input[aria-label='Engraving - Ski Pole 1']");
-          var engravingInput2 = document.querySelector("input[aria-label='Engraving - Ski Pole 2']");
-          var addMoreDiv = document.querySelector(".form-control.form-control--button.form-control--large.form-control--secondary.form-control--flexible.form-control--animated.details-product-purchase__add-more.form-control__button--icon-center.form-control--done");
-          var addToBagDiv = document.querySelector(".form-control.form-control--button.form-control--large.form-control--primary.form-control--flexible.form-control--animated.details-product-purchase__add-to-bag.form-control__button--icon-center.form-control--done");
+          const engravingInput1 = document.querySelector("input[aria-label='Engraving - Ski Pole 1']");
+          const engravingInput2 = document.querySelector("input[aria-label='Engraving - Ski Pole 2']");
+          const addMoreDiv = document.querySelector(".form-control.form-control--button.form-control--large.form-control--secondary.form-control--flexible.form-control--animated.form-control--done.details-product-purchase__add-more");
+          const addToBagDiv = document.querySelector(".form-control.form-control--button.form-control--large.form-control--primary.form-control--flexible.form-control--animated.form-control--done.details-product-purchase__add-to-bag");
   
           // Function to update the price
           function updatePrice() {
-              var engravingText1 = engravingInput1 ? engravingInput1.value : '';
-              var engravingText2 = engravingInput2 ? engravingInput2.value : '';
+            try {
+              const engravingText1 = engravingInput1 ? engravingInput1.value : '';
+              const engravingText2 = engravingInput2 ? engravingInput2.value : '';
   
               // STRAP
               const strapRadio = document.querySelector("input[name='Strap']:checked");
@@ -34,36 +35,41 @@ Ecwid.OnAPILoaded.add(function() {
   
               const ind=[0,18,18,18,18,18,18,19.75,19.75,21.5,21.5,23.25,23.25,25,25,26.75,26.75,28.5,28.5,30.25,30.25,32,32,33.75,33.75,35.5,35.5,37.25,37.25,39,39,40.75,40.75,42.5,42.5,44.25,44.25,46,46,47.75,47.75];
   
-              var charCount = engravingText1.length + engravingText2.length;
-              var engravingCost = ind[charCount];
+              const charCount = engravingText1.length + engravingText2.length;
+              const engravingCost = ind[charCount];
   
               // Update the displayed price
-              var priceElement = document.querySelector('.details-product-price__value.ec-price-item.notranslate');
-              var gripPrice = (gripColorValue === 'Cork') ? 14 : 0;
-              var strapPrice;
+              const priceElement = document.querySelector('.details-product-price__value.ec-price-item.notranslate');
+              const gripPrice = (gripColorValue === 'Cork') ? 14 : 0;
+              let strapPrice;
               if (strapValue === 'None') {strapPrice = -3;}
               else if (strapValue === 'Adjustable') {strapPrice = 10;}
               else if (strapValue === 'Fixed') {strapPrice = 0;}
               else {strapPrice = 19.99;}
-              var newPrice = basePrice + engravingCost + gripPrice + strapPrice;
+              const newPrice = basePrice + engravingCost + gripPrice + strapPrice;
   
               if (priceElement) {
                   priceElement.textContent = `$${newPrice.toFixed(2)}`;
                   console.log(`price being updated to $${newPrice.toFixed(2)}`);
               }
+            }
+            catch (error) {
+              console.error('Error updating price:', error);
+            }
           }
   
           // Function to update product options on add to cart
           function handleAddToCart(event) {
+            return new Promise((resolve, reject) => {
               console.log("handle add to cart active");
               event.preventDefault();  // Prevent the default add to cart behavior
               
               // Engraving
               const customEngraving = ['0', '1-6', '1-6', '1-6', '1-6', '1-6', '1-6', '7-8', '7-8', '9-10', '9-10', '11-12', '11-12', '13-14', '13-14', '15-16', '15-16', '17-18', '17-18', '19-20', '19-20', '21-22', '21-22', '23-24', '23-24', '25-26', '25-26', '27-28', '27-28', '29-30', '29-30', '31-32', '31-32', '33-34', '33-34', '35-36', '35-36', '37-38', '37-38', '39-40', '39-40'];
-              var engravingText1 = engravingInput1 ? engravingInput1.value : '';
-              var engravingText2 = engravingInput2 ? engravingInput2.value : '';
-              var charCount = engravingText1.length + engravingText2.length;
-              var engravingCost = customEngraving[charCount];
+              const engravingText1 = engravingInput1 ? engravingInput1.value : '';
+              const engravingText2 = engravingInput2 ? engravingInput2.value : '';
+              const charCount = engravingText1.length + engravingText2.length;
+              const engravingCost = customEngraving[charCount];
               console.log('engraving cost:', engravingCost);
   
               // BASKET SIZE
@@ -104,14 +110,14 @@ Ecwid.OnAPILoaded.add(function() {
               const lengthInput = document.querySelector("input[aria-label='Length (cm or inches)']");
               const lengthInputVal = lengthInput ? lengthInput.value : '';
               console.log('length:', lengthInputVal);
-              if (lengthInputVal==='') {
-                  console.log('Length input not present');
-                  return;
+              if (lengthInputVal === '') {
+                console.log('Length input not present');
+                return reject(new Error('Length input is required'));
               }
   
               // Quantity
               const quantityCheck = document.querySelector("input[name='ec-qty']");
-              var quantityValue = quantityCheck ? parseInt(quantityCheck.value, 10) : 1;
+              let quantityValue = quantityCheck ? parseInt(quantityCheck.value, 10) : 1;
               if (isNaN(quantityValue)) {
                   quantityValue = 1;
                   console.log('quantity default is not a number');
@@ -119,7 +125,7 @@ Ecwid.OnAPILoaded.add(function() {
               console.log('quantity:', quantityValue, 'and type:', typeof quantityValue);
   
               // Prepare the product options to include the engraving cost
-              var options = {
+              const options = {
                   'Basket Size': `${basketSize}`,
                   'Grip Color': `${gripColor}`,
                   'Basket Color': `${basketColor}`,
@@ -133,17 +139,31 @@ Ecwid.OnAPILoaded.add(function() {
               console.log('quantity:', quantityValue);
               
               // Add the product to the cart with the engraving option
-              Ecwid.Cart.removeProduct(-1);
               Ecwid.Cart.addProduct({
                   id: page.productId,
                   quantity: quantityValue,
                   options: options,
                   callback: function(success, product, cart, error) {
-                      console.log('success', success);
-                      console.log('product:', product);
-                      console.log('cart:', cart);
-                      console.log('error:', error);
+                    console.log('success', success);
+                    console.log('product:', product);
+                    console.log('cart:', cart);
+                    console.log('error:', error);
+                    
+                    if (success) {
+                      resolve(product);
+                    } else {
+                      reject(error || new Error('Failed to add product to cart'));
+                    }
                   }
+              });
+            });
+          }
+
+          function handleRemoveFromCart() {
+            return new Promise(resolve => {
+                Ecwid.Cart.removeProduct(-1);
+                console.log('Product removed from cart');
+                resolve();
               });
           }
   
@@ -174,18 +194,36 @@ Ecwid.OnAPILoaded.add(function() {
   
           // Attach the click event listener to the Add to Cart button
           if (addToBagDiv) {
-              var addToCartButton = addToBagDiv.querySelector(".form-control__button");
-              if (addToCartButton) {
-                  console.log('Add to Bag button present');
-                  addToCartButton.addEventListener('click', handleAddToCart);
-              }
+            const addToCartButton = addToBagDiv.querySelector(".form-control__button.form-control__button--icon-center");
+            if (addToCartButton) {
+              console.log('Add to Bag button present');
+              addToCartButton.addEventListener('click', async (event) => {
+                try {
+                  await handleAddToCart(event);
+                  console.log('Product added to cart successfully');
+                  await handleRemoveFromCart();
+                  console.log('Product removed from cart');
+                } catch (error) {
+                  console.error('Error handling add to cart:', error);
+                }
+              });
+            }
           }
           if (addMoreDiv) {
-              var addMoreButton = addMoreDiv.querySelector(".form-control__button");
-              if (addMoreButton) {
-                  console.log('Add More button present');
-                  addMoreButton.addEventListener('click', handleAddToCart);
-              }
+            const addMoreButton = addMoreDiv.querySelector(".form-control__button.form-control__button--icon-center");
+            if (addMoreButton) {
+              console.log('Add More button present');
+              addMoreButton.addEventListener('click', async (event) => {
+                try {
+                  await handleAddToCart(event);
+                  console.log('Product added to cart successfully');
+                  await handleRemoveFromCart();
+                  console.log('Product removed from cart');
+                } catch (error) {
+                  console.error('Error handling add to cart:', error);
+                }
+              });
+            }
           }
       }
     });
